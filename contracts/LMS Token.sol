6cf812bf7fb2d0414637afb0335187f94521ec4b
Peter Kyale <kyalepeter2000@gmail.com>
2024-11-29 16:30:55 +0300

    added an LMSToken contract & tested it

---
+++ contracts/LMS Token.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+contract LMSToken is ERC20, AccessControl {
+    uint256 public constant MAX_SUPPLY = 10000000000 * 10 ** 18;
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+
+    event MintSuccess(address indexed _to, uint256 indexed _amount);
+    event BurnSuccess(address indexed _account, uint256 _amount);
+
+    constructor(address[] memory _admins) ERC20("ABYA TOKEN", "ABYTKN") {
+        require(_admins.length >= 2, "At least 2 admins are required");
+
+        // Assign the deployer the default admin role
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+
+        // Grant each admin the ADMIN_ROLE
+        for (uint256 i = 0; i < _admins.length; i++) {
+            _grantRole(ADMIN_ROLE, _admins[i]);
+        }
+    }
+
+    // Mint function
+    function mintToken(address to, uint256 amount) internal  returns (bool) {
+        require(amount + totalSupply() <= MAX_SUPPLY, "Limit Exceeded!");
+        _mint(to, amount);
+        emit MintSuccess(to, amount);
+        return true;
+    }
+
+    // Burn function
+    function burn(address account, uint256 amount) internal {
+        require(balanceOf(account) >= amount, "Insufficient balance");
+        _burn(account, amount);
+        emit BurnSuccess(account, amount);
+    }
+
+    // Add a new admin
+    function addAdmin(address newAdmin) external onlyRole(ADMIN_ROLE) {
+        _grantRole(ADMIN_ROLE, newAdmin);
+    }
+
+    // Remove an admin (restricted to DEFAULT_ADMIN_ROLE)
+    function removeAdmin(address admin) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        revokeRole(ADMIN_ROLE, admin);
+    }
+
+    // Check if an address has admin privileges
+    function isAdmin(address account) public view returns (bool) {
+        return hasRole(ADMIN_ROLE, account);
+    }
+}
---
+++ test/lmsTokenTest.js
@@ -0,0 +1,56 @@
+/* eslint-env mocha */
+
+import { expect } from "chai";
+import pkg from 'hardhat';
+import { describe, it, beforeEach } from 'mocha';
+const { ethers } = pkg;
+
+describe("LMSToken", function () {
+    let LMSToken, owner, addr1, addr2, addr3;
+
+    beforeEach("Run Before All", async function () {
+        LMSToken = await ethers.getContractFactory("LMSToken");
+        [owner, addr1, addr2, addr3] = await ethers.getSigners();
+        this.lmstoken = await LMSToken.deploy([owner.address, addr1.address, addr2.address]);
+    });
+
+    describe("Deployment", async function () {
+        it("Should set the right owner", async function () {
+            expect(await this.lmstoken.hasRole(this.lmstoken.DEFAULT_ADMIN_ROLE(), owner.address)).to.be.true;
+        });
+
+        it("Should set the right token name", async function () {
+            expect(await this.lmstoken.name()).to.equal("ABYA TOKEN");
+        });
+
+        it("Should set the right token symbol", async function () {
+            const symbol = await this.lmstoken.symbol();
+            expect(symbol).to.equal("ABYTKN");
+        });
+
+        it("Should set the admins correctly", async function () {
+            const isAdmin1 = await this.lmstoken.hasRole(this.lmstoken.ADMIN_ROLE(), addr1.address);
+            const isAdmin2 = await this.lmstoken.hasRole(this.lmstoken.ADMIN_ROLE(), addr2.address);
+            expect(isAdmin1).to.be.true;
+            expect(isAdmin2).to.be.true;
+        });
+    });
+
+    describe("Add admin", async function () {
+        it("Should add an admin successfully", async function () {
+            expect(await this.lmstoken.hasRole(this.lmstoken.ADMIN_ROLE(), addr1.address)).to.be.true;
+            await this.lmstoken.connect(addr1).addAdmin(addr3.address);
+
+            const isAdmin3 = await this.lmstoken.hasRole(this.lmstoken.ADMIN_ROLE(), addr3.address);
+            await expect(isAdmin3).to.be.true;
+        })
+
+        it("Should remove an admin successfully", async function () {
+            await this.lmstoken.connect(owner).removeAdmin(addr1.address);
+
+            const isAdmin1 = await this.lmstoken.hasRole(this.lmstoken.ADMIN_ROLE(), addr3.address);
+            await expect(isAdmin1).to.be.false;
+        })
+    })
+
+});
