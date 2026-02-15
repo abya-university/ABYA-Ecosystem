64fdb5c70ed345a8d76ecd2a6e6ac426f183adc6
peter-mwau <kyalepeter2000@gmail.com>
2026-02-15 12:58:08 +0300

    added the 3 new facet artifacts

--- src/constants/addresses.js
+++ src/constants/addresses.js
@@ -6,12 +6,15 @@ const CONTRACT_ADDRESSES = {
         OwnershipFacet: "0xC08aEE41751a258dBE71Df8f9eBD1c527d24AB35",
         TokenManagementFacet: "0xF3Dd1E07298861FA33570542e317c381e8498630",
         Ecosystem1Facet: "0xd00B1060F3f681B9E87E43295A0E960380fe271e",
-        Ecosystem2Facet: "0xB8Ae6Df66492FD8596dE724C3341aa6Dcf6f2953",
+        Ecosystem2Facet: "0x0590f55e05df597FaFb41720F39CB2FCa65ce291",
         Ecosystem3Facet: "0x0661D59B524618bC3b9a086bc0862d839B46bD9C",
         CommunityEngagementFacet: "0xA98dC21c8F9835089eeDfC7a4a406D50f5d3AF74",
         CommunityGovernanceFacet: "0x134527fDb572EcE377940f622A7e538885b135F2",
         CommunityBadgesFacet: "0xe7B2e77D448eA59eB29B6F320c81A9F05687aD18",
-        ProfileFacet: "0xbEa20409b1FFa4a56635E6d44757F99911005AaF"
+        ProfileFacet: "0xbEa20409b1FFa4a56635E6d44757F99911005AaF",
+        AmbassadorNetworkFacet: "0x212D2bB1bDBd0d3123a07421a678B9b17F006c67",
+        RevenueSharingFacet: "0x9210dBb58e07B8eD79774100E7C1463c92406D97",
+        VestingFacet: "0xDBC76B96611bcB68537Ee52593D53B720fF7e1bA"
     },
     init: {
         DiamondInit: "0x0acCd49E6326c95E964b0C009eA031f238818aB4"
